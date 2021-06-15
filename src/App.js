151acab63615d250962eca61dc65174bc3836ccb
Angel <araozangel842@gmail.com>
2021-06-15 13:20:51 +0300

    est changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Angel desde React</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum asperiores possimus dicta reiciendis eveniet a, neque quas, corrupti ipsam officia quaerat totam laudantium, deserunt ullam! Illo beatae eveniet itaque sed.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
